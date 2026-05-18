e52ce74f9b6e228c7a11f036fc969df833441625
keita811627 <pan.811627@gmail.com>
2026-05-18 22:19:23 +0900

    first commit

---
+++ js/common.js
@@ -0,0 +1,40 @@
+﻿window.addEventListener("DOMContentLoaded", () => {
+    fetch("header.html")
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById("header").innerHTML = data;
+        });
+
+    fetch("footer.html")
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById("footer").innerHTML = data;
+        });
+
+    fetch("../header.html")
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById("achieveHeader").innerHTML = data;
+        });
+
+    fetch("../footer.html")
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById("achieveFooter").innerHTML = data;
+        });
+});
+
+$(function () {
+
+    $("#testButton").click(function () {
+
+        alert("クリックされました");
+
+    });
+
+});
+
+function home() {
+    location.href = '../index.html';
+}
+
---
+++ js/header.js
@@ -0,0 +1,11 @@
+﻿function toggleItem() {
+    var menu = document.getElementById("toggleItem");
+    var btn = document.getElementById("headerItemBtn");
+    btn.classList.add("hidden");
+
+    if (menu.style.display === "block") {
+        menu.style.display = "none";
+    } else {
+        menu.style.display = "block";
+    }
+}
